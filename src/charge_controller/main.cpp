ebac66bad1e80dc70b25a9b62d89427f811c3417
mjh9585 <69865851+mjh9585@users.noreply.github.com>
2022-02-19 11:35:04 -0500

    moved pin definitions to constants and set platform to compile for

--- src/charge_controller/main.cpp
+++ src/charge_controller/main.cpp
@@ -15,6 +15,17 @@ namespace time = EVT::core::time;
 #define MAX_VOLTAGE 12
 #define MAX_TEMPERATURE 90.0
 
+constexpr IO::Pin RELAY_CTL_PIN = IO::Pin::PA_1;
+constexpr IO::Pin LED_PIN = IO::Pin::PA_2;
+constexpr IO::Pin ON_OFF_BUTTON_PIN = IO::Pin::PA_3;
+constexpr IO::Pin LCD_A0_PIN = IO::Pin::PA_4;
+
+constexpr IO::Pin UART_RX_PIN = IO::Pin::PA_10;
+constexpr IO::Pin UART_TX_PIN = IO::Pin::PA_11;
+
+constexpr IO::Pin CAN_RX_PIN = IO::Pin::PC_4;
+constexpr IO::Pin CAN_TX_PIN = IO::Pin::PC_5;
+
 void disconnectBattery(){
 
 }
@@ -44,12 +55,11 @@ int main(){
 
     constexpr uint8_t BMS_CAN_ADDRESS = 0x00;
 
-
-    IO::UART& uart = IO::getUART<IO::Pin::PA_11, IO::Pin::PA_10>(9600);
-    IO::CAN& can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>();
-    IO::GPIO& relayControl = IO::getGPIO<EVT::core::IO::Pin::PA_1>();
-    IO::GPIO& statusLED = IO::getGPIO<EVT::core::IO::Pin::PA_2>();
-    IO::GPIO& LCDRegisterSEL = IO::getGPIO<EVT::core::IO::Pin::PA_4>();
+    IO::UART& uart = IO::getUART<UART_TX_PIN, UART_RX_PIN>(9600);
+    IO::CAN& can = IO::getCAN<CAN_TX_PIN, CAN_RX_PIN>();
+    IO::GPIO& relayControl = IO::getGPIO<RELAY_CTL_PIN>();
+    IO::GPIO& statusLED = IO::getGPIO<LED_PIN>();
+    IO::GPIO& LCDRegisterSEL = IO::getGPIO<LCD_A0_PIN>();
 
     BMSManager bms = BMSManager(can, BMS_CAN_ADDRESS);
 
