724970fdc1b4530e9dce0002bf3413436d01645d
mjh9585 <69865851+mjh9585@users.noreply.github.com>
2022-04-09 10:58:32 -0400

    created button debouncer

---
+++ src/charge_controller/Debounce.cpp
@@ -0,0 +1,18 @@
+#include <charge_controller/dev/Debounce.hpp>
+
+Debounce::Debounce(EVT::core::IO::GPIO &pin) {
+    this->pin = pin;
+    lastState = pin.readPin();
+    state = lastState;
+}
+
+bool Debounce::read() {
+    if(pin.readPin() != lastState){
+        lastPressed = time::millis();
+        lastState = pin.readPin();
+    }else if(time::millis() - lastPressed > debounceTime){
+        state = lastState;
+    }
+
+    return state == pressedState;
+}
