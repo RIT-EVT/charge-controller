8fcfae4ad19083ad7f285daaf17881dd4ee95254
Matthew Heller <mjh9585@rit.edu>
2022-11-20 02:28:08 -0500

    Setup BMS RPDOs for 1 cell

--- include/charge_controller/dev/BMSManager.hpp
+++ include/charge_controller/dev/BMSManager.hpp
@@ -1,18 +1,17 @@
 #ifndef EXAMPLE_BMSMANAGER_HPP
 #define EXAMPLE_BMSMANAGER_HPP
 
+#include <stdint.h>
 #include <EVT/io/CAN.hpp>
-#include <charge_controller/Logger.h>
 #include <EVT/io/GPIO.hpp>
 
 namespace IO = EVT::core::IO;
+
 class BMSManager {
     static constexpr uint32_t TOTAL_VOLTAGE_ID = 0x2101;
     static constexpr uint32_t STATE_ID = 0x2102;
 public:
-    BMSManager(IO::CAN& can, IO::GPIO& bmsOK1, IO::GPIO& bmsOK2);
-
-    enum class BMSState {
+    enum class BMSStatus {
         /// When the BMS is powered on
         START = 0,
         /// When the BMS fails startup sequence
@@ -33,21 +32,341 @@ public:
         CHARGING = 8
     };
 
-    void updateBMSData(IO::CANMessage& message);
-    int getBattVoltage() {return millivolts;};
-    int getBattTemperature() { return 0; };
-    BMSState getBMSState(){ return state; };
-    int getStateOfCharge();
-    bool isConnected();
-    bool faultDetected();
+    BMSManager(IO::CAN &can, IO::GPIO *bmsOK[]);
 
+//    void updateBMSData(IO::CANMessage &message);
+//
+//    int getBattVoltage() { return millivolts; };
+//
+//    int getBattTemperature() { return 0; };
+//
+//    BMSStatus getBMSState() { return state; };
+//
+//    int getStateOfCharge();
+
+    bool isConnected(uint8_t packNum);
+
+    uint8_t numConnected();
+
+    bool faultDetected(uint8_t packNum);
+
+    bool isCharging(uint8_t packNum);
+
+    bool isReady(uint8_t packNum);
+
+    int16_t getBatteryVoltage(uint8_t packNum);
+
+    int16_t getMinCellVoltage(uint8_t packNum);
+
+    uint8_t getMinCellVoltageID(uint8_t packNum);
+
+    int16_t getMaxCellVoltage(uint8_t packNum);
+
+    uint8_t getMaxCellVoltageID(uint8_t packNum);
+
+    int8_t getBatteryMinTemp(uint8_t packNum);
+
+    int8_t getBatteryMaxTemp(uint8_t packNum);
+
+    BMSStatus getStatus(uint8_t packNum);
+
+    void printDebug();
+
+    void update();
+
+    /**
+     * Get a pointer to the start of the object dictionary
+     *
+     * @return Pointer to the start of the object dictionary
+     */
+    CO_OBJ_T* getObjectDictionary();
+
+    /**
+     * Get the number of elements in the object dictionary.
+     *
+     * @return The number of elements in the object dictionary
+     */
+    uint8_t getNumElements();
+
+    static constexpr uint8_t MAX_BMS_PACKS = 1;
 private:
-    IO::CAN* can = nullptr;
-    IO::GPIO& bmsOK1;
-    IO::GPIO& bmsOK2;
-    uint32_t millivolts = 0;
-    uint32_t temperature = 5;
-    BMSState state = BMSState::START;
+    /**
+     * State for representing the BMS is in not in an OK state to charge/discharge
+     */
+    static constexpr EVT::core::IO::GPIO::State BMS_OK =
+            EVT::core::IO::GPIO::State::HIGH;
+
+    /**
+     * State for representing the BMS is in not in an OK state to charge/discharge
+     */
+    static constexpr EVT::core::IO::GPIO::State BMS_NOT_OK =
+            EVT::core::IO::GPIO::State::LOW;
+
+    IO::CAN *can = nullptr;
+    IO::GPIO *bmsOK[MAX_BMS_PACKS];
+//    IO::GPIO &bmsOK2;
+
+//    uint32_t millivolts = 0; //TODO Replace
+//    uint32_t temperature = 5; //TODO Replace
+//    BMSState state = BMSState::START; //TODO Replace
+
+    struct BMSData {
+        int16_t batteryVoltage;
+        int16_t minCellVoltage;
+        uint8_t minCellVoltageID;
+        int16_t maxCellVoltage;
+        uint8_t maxCellVoltageID;
+
+        int8_t batteryPackMinTemp;
+        int8_t batteryPackMaxTemp;
+        BMSStatus status;
+    };
+
+    struct BMSConfig {
+        uint8_t node_ID;
+        bool isConnected;
+        BMSData data;
+    };
+
+    struct BMSConfig packs[MAX_BMS_PACKS] = {
+            {0x05, false},
+//            {0x24, false}
+    };
+
+    BMSData lastValues[MAX_BMS_PACKS];
+
+    uint8_t dummy = 0;
+
+    static constexpr uint8_t NODE_ID = 0x0F;
+    static constexpr uint16_t OBJECT_DICTIONARY_SIZE = 36;
+
+    CO_OBJ_T objectDictionaryBMS[OBJECT_DICTIONARY_SIZE + 1] = {
+        // Sync ID, defaults to 0x80
+        {
+            .Key = CO_KEY(0x1005, 0, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0x80,
+        },
+
+        //Heartbeat Producer
+//        {
+//            .Key = CO_KEY(0x1017, 0, CO_UNSIGNED32 | CO_OBJ_D__R_),
+//            .Type = nullptr,
+//            .Data = (uintptr_t) 500,
+//        },
+
+        // Information about the hardware, hard coded sample values for now
+        // 1: Vendor ID
+        // 2: Product Code
+        // 3: Revision Number
+        // 4: Serial Number
+        {
+            .Key = CO_KEY(0x1018, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0x10},
+        {
+            .Key = CO_KEY(0x1018, 2, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0x11,
+        },
+        {
+            .Key = CO_KEY(0x1018, 3, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0x12,
+        },
+        {
+            .Key = CO_KEY(0x1018, 4, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0x13,
+        },
+
+        // SDO CAN message IDS.
+        // 1: Client -> Server ID, default is 0x600 + NODE_ID
+        // 2: Server -> Client ID, default is 0x580 + NODE_ID
+        {
+            .Key = CO_KEY(0x1200, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0x600 + NODE_ID,
+        },
+        {
+            .Key = CO_KEY(0x1200, 2, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0x580 + NODE_ID,
+        },
+
+        //RPDO0 settings
+        // 0: RPDO number in index and total number of sub indexes.
+        // 1: The COB-ID to receive PDOs from.
+        // 2: transmission trigger
+        {
+            .Key = CO_KEY(0x1400, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 3,
+        },
+        {
+            .Key = CO_KEY(0x1400, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0) + packs[0].node_ID,
+        },
+        {
+            .Key = CO_KEY(0x1400, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0xFE,
+        },
+
+        //RPDO1 settings
+        // 0: RPDO number in index and total number of sub indexes.
+        // 1: The COB-ID to receive PDOs from.
+        // 2: transmission trigger
+        {
+            .Key = CO_KEY(0x1401, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 3,
+        },
+        {
+            .Key = CO_KEY(0x1401, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0) + packs[0].node_ID + 1,
+        },
+        {
+            .Key = CO_KEY(0x1401, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 0xFE,
+        },
+
+        // RPDO0 mapping, determines the PDO messages to send when TPDO1 is triggered
+        // 0: The number of PDO messages associated with the TPDO
+        // 1: Link to the first PDO message
+        // n: Link to the nth PDO message
+        {
+            .Key = CO_KEY(0x1600, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 5,
+        },
+        { // Battery Voltage
+            .Key = CO_KEY(0x1600, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 1, 16),
+        },
+        { // Min Cell Voltage
+            .Key = CO_KEY(0x1600, 2, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 2, 16),
+        },
+        { // Min Cell Voltage ID
+            .Key = CO_KEY(0x1600, 3, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 3, 8),
+        },
+        { // Max Cell Voltage
+            .Key = CO_KEY(0x1600, 4, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 4, 16),
+        },
+        { // Max Cell Voltage ID
+            .Key = CO_KEY(0x1600, 5, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 5, 8),
+        },
+
+        // RPDO1 mapping, determines the PDO messages to send when TPDO1 is triggered
+        // 0: The number of PDO messages associated with the TPDO
+        // 1: Link to the first PDO message
+        // n: Link to the nth PDO message
+        {
+            .Key = CO_KEY(0x1601, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = (uintptr_t) 7,
+        },
+        { // Current - Unused
+            .Key = CO_KEY(0x1601, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x5000, 0, 8),
+        },
+        { // Pack Min Temp
+            .Key = CO_KEY(0x1601, 2, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 7, 8),
+        },
+        { // Pack Max Temp
+            .Key = CO_KEY(0x1601, 3, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 8, 8),
+        },
+        { // SOC - Unused
+            .Key = CO_KEY(0x1601, 4, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x5000, 0, 8),
+        },
+        { // State
+            .Key = CO_KEY(0x1601, 5, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x2100, 10, 8),
+        },
+        { //RecapActualAllowed - Unused
+            .Key = CO_KEY(0x1601, 6, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x5000, 0, 8),
+        },
+        { //DischargeActualAllowed - Unused
+            .Key = CO_KEY(0x1601, 7, CO_UNSIGNED32 | CO_OBJ_D__R_),
+            .Type = nullptr,
+            .Data = CO_LINK(0x5000, 0, 8),
+        },
+
+        // User defined data, this will be where we put elements that can be
+        // accessed via SDO and depending on configuration PDO
+
+        //Pack 1 Data
+        {
+            .Key = CO_KEY(0x2100, 1, CO_UNSIGNED16 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.batteryVoltage,
+        },
+        {
+            .Key = CO_KEY(0x2100, 2, CO_UNSIGNED16 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.minCellVoltage,
+        },
+        {
+            .Key = CO_KEY(0x2100, 3, CO_UNSIGNED8 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.minCellVoltageID,
+        },
+        {
+            .Key = CO_KEY(0x2100, 4, CO_UNSIGNED16 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.maxCellVoltage,
+        },
+        {
+            .Key = CO_KEY(0x2100, 5, CO_UNSIGNED8 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.maxCellVoltageID,
+        },
+        {
+            .Key = CO_KEY(0x2100, 7, CO_SIGNED8 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.batteryPackMinTemp,
+        },
+        {
+            .Key = CO_KEY(0x2100, 8, CO_SIGNED8 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.batteryPackMaxTemp,
+        },
+        {
+            .Key = CO_KEY(0x2100, 10, CO_UNSIGNED8 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &packs[0].data.status,
+        },
+        {
+            .Key = CO_KEY(0x5000, 0, CO_UNSIGNED8 | CO_OBJ___PR_),
+            .Type = nullptr,
+            .Data = (uintptr_t) &dummy,
+        },
+
+        // End of dictionary marker
+        CO_OBJ_DIR_ENDMARK,
+    };
 };
 
 #endif// EXAMPLE_BMSMANAGER_HPP
--- src/charge_controller/dev/BMSManager.cpp
+++ src/charge_controller/dev/BMSManager.cpp
@@ -1,30 +1,138 @@
 
 #include <charge_controller/dev/BMSManager.hpp>
 
-bool BMSManager::isConnected() { return bmsOK2.readPin() == IO::GPIO::State::HIGH; }
 
-bool BMSManager::faultDetected() { return bmsOK1.readPin() == IO::GPIO::State::LOW; }
 
-BMSManager::BMSManager(IO::CAN& can, IO::GPIO& bmsOK1, IO::GPIO& bmsOK2) : bmsOK1(bmsOK1), bmsOK2(bmsOK2){
+BMSManager::BMSManager(IO::CAN& can, IO::GPIO *bmsOK[]) {
     this->can = &can;
+    for (int i = 0; i < MAX_BMS_PACKS; i++) {
+        this->bmsOK[i] = bmsOK[i];
+    }
 };
 
-int BMSManager::getStateOfCharge() { return 0; }
+//int BMSManager::getStateOfCharge() { return 0; }
+//
+//void BMSManager::updateBMSData(IO::CANMessage& message) {
+//    uint8_t length = message.getDataLength();
+//    LOG.log(Logger::DEBUG, "Got can message with id 0x%X and length %u", message.getId(),message.getDataLength());
+//    if(message.getId() == TOTAL_VOLTAGE_ID){
+//
+//        if(length == 4){
+//            millivolts = *((uint32_t*) message.getPayload());
+//            LOG.log(Logger::DEBUG,"Got voltage update %u", millivolts);
+//        }
+//    } else if(message.getId() == STATE_ID){
+//        if(length == 1){
+//            BMSStatus data = *((BMSStatus*) message.getPayload());
+//            LOG.log(Logger::DEBUG,"Got state update %u", data);
+//            BMSStatus state = data;
+//        }
+//    }
+//}
 
-void BMSManager::updateBMSData(IO::CANMessage& message) {
-    uint8_t length = message.getDataLength();
-    LOG.log(Logger::DEBUG, "Got can message with id 0x%X and length %u", message.getId(),message.getDataLength());
-    if(message.getId() == TOTAL_VOLTAGE_ID){
+uint8_t BMSManager::numConnected() {
+    uint8_t count = 0;
+    for(int i = 0; i< MAX_BMS_PACKS; i++){
+        if(packs[i].isConnected){
+            count++;
+        }
+    }
+    return count;
+}
 
-        if(length == 4){
-            millivolts = *((uint32_t*) message.getPayload());
-            LOG.log(Logger::DEBUG,"Got voltage update %u", millivolts);
+bool BMSManager::isConnected(uint8_t packNum) {
+    return packs[packNum].isConnected;
+}
+
+bool BMSManager::faultDetected(uint8_t packNum) {
+    return bmsOK[packNum]->readPin() == BMS_NOT_OK ||
+           packs[packNum].data.status == BMSStatus::UNSAFE_CONDITIONS_ERROR ||
+           packs[packNum].data.status == BMSStatus::INITIALIZATION_ERROR;
+}
+
+bool BMSManager::isCharging(uint8_t packNum) {
+    return packs[packNum].data.status == BMSStatus::CHARGING;
+}
+
+bool BMSManager::isReady(uint8_t packNum) {
+    return  packs[packNum].data.status == BMSStatus::SYSTEM_READY ||
+            packs[packNum].data.status == BMSStatus::DEEP_SLEEP ||
+            packs[packNum].data.status == BMSStatus::CHARGING;
+}
+
+int16_t BMSManager::getBatteryVoltage(uint8_t packNum) {
+    return packs[packNum].data.batteryVoltage;
+}
+
+int16_t BMSManager::getMinCellVoltage(uint8_t packNum) {
+    return packs[packNum].data.minCellVoltage;
+}
+
+uint8_t BMSManager::getMinCellVoltageID(uint8_t packNum) {
+    return packs[packNum].data.minCellVoltageID;
+}
+
+int16_t BMSManager::getMaxCellVoltage(uint8_t packNum) {
+    return packs[packNum].data.maxCellVoltage;
+}
+
+uint8_t BMSManager::getMaxCellVoltageID(uint8_t packNum) {
+    return packs[packNum].data.maxCellVoltageID;
+}
+
+int8_t BMSManager::getBatteryMinTemp(uint8_t packNum) {
+    return packs[packNum].data.batteryPackMinTemp;
+}
+
+int8_t BMSManager::getBatteryMaxTemp(uint8_t packNum) {
+    return packs[packNum].data.batteryPackMaxTemp;
+}
+
+BMSManager::BMSStatus BMSManager::getStatus(uint8_t packNum) {
+    return packs[packNum].data.status;
+}
+
+uint8_t BMSManager::getNumElements() {
+    return OBJECT_DICTIONARY_SIZE;
+}
+
+CO_OBJ_T *BMSManager::getObjectDictionary() {
+    return &objectDictionaryBMS[0];
+}
+
+
+void BMSManager::printDebug() {
+    for(int i = 0; i<MAX_BMS_PACKS; i++) {
+        if (packs[i].data.batteryVoltage != lastValues[i].batteryVoltage) {
+            LOG.log(Logger::DEBUG, "Pack%d->batteryVoltage=%d",i, packs[i].data.batteryVoltage);
         }
-    } else if(message.getId() == STATE_ID){
-        if(length == 1){
-            BMSState data = *((BMSState*) message.getPayload());
-            LOG.log(Logger::DEBUG,"Got state update %u", data);
-            BMSState state = data;
+        if (packs[i].data.minCellVoltage != lastValues[i].minCellVoltage) {
+            LOG.log(Logger::DEBUG, "Pack%d->minCellVoltage=%d",i, packs[i].data.minCellVoltage);
         }
+        if (packs[i].data.minCellVoltageID != lastValues[i].minCellVoltageID) {
+            LOG.log(Logger::DEBUG, "Pack%d->minCellVoltageID=%d",i, packs[i].data.minCellVoltageID);
+        }
+        if (packs[i].data.maxCellVoltage != lastValues[i].maxCellVoltage) {
+            LOG.log(Logger::DEBUG, "Pack%d->maxCellVoltage=%d",i, packs[i].data.maxCellVoltage);
+        }
+        if (packs[i].data.maxCellVoltageID != lastValues[i].maxCellVoltageID) {
+            LOG.log(Logger::DEBUG, "Pack%d->maxCellVoltageID=%d",i, packs[i].data.maxCellVoltageID);
+        }
+        if (packs[i].data.batteryPackMinTemp != lastValues[i].batteryPackMinTemp) {
+            LOG.log(Logger::DEBUG, "Pack%d->batteryPackMinTemp=%d",i, packs[i].data.batteryPackMinTemp);
+        }
+        if (packs[i].data.batteryPackMaxTemp != lastValues[i].batteryPackMaxTemp) {
+            LOG.log(Logger::DEBUG, "Pack%d->batteryPackMaxTemp=%d",i, packs[i].data.batteryPackMaxTemp);
+        }
+        if (packs[i].data.status != lastValues[i].status) {
+            LOG.log(Logger::DEBUG, "Pack%d->status=%d",i, packs[i].data.status);
+        }
+        lastValues[i] = packs[i].data;
+    }
+}
+
+void BMSManager::update() {
+    for(int i = 0; i< MAX_BMS_PACKS; i++){
+        packs[i].isConnected = bmsOK[i]->readPin() == IO::GPIO::State::HIGH;
     }
 }
